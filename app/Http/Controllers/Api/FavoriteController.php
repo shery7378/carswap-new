ae8c29d386aef6eecbba2c438327ba6dcc572daa
Mujtaba <03mujtabaasad@gmail.com>
2026-05-13 17:10:55 +0500

    Add status update in FavoriteController

--- app/Http/Controllers/Api/FavoriteController.php
+++ app/Http/Controllers/Api/FavoriteController.php
@@ -33,7 +33,7 @@ class FavoriteController extends Controller
         $favorites = $request->user()
             ->favorites()
             ->with($this->relations)
-            ->where('ad_status', 'Published')
+            ->where('ad_status', 'Publikált')
             ->orderBy('favorites.created_at', 'desc')
             ->paginate($request->input('limit', 12));
 
